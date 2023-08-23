58d517e2a341fc1edad61c839c06d9ba497d861a
Allyson-English <allyson.english@gmail.com>
2023-08-23 11:05:28 -0600

    update settings w necessary fields

--- internal/config/settings.go
+++ internal/config/settings.go
@@ -4,18 +4,22 @@ import "fmt"
 
 // Settings credentials
 type Settings struct {
-	DBUser                         string `yaml:"DB_USER"`
-	DBPassword                     string `yaml:"DB_PASSWORD"`
-	DBPort                         string `yaml:"DB_PORT"`
-	DBHost                         string `yaml:"DB_HOST"`
-	DBName                         string `yaml:"DB_NAME"`
-	KafkaBrokers                   string `yaml:"KAFKA_BROKERS"`
-	JWTKeySetURL                   string `yaml:"JWT_KEY_SET_URL"`
-	DevicesAPIGRPCAddr             string `yaml:"DEVICES_API_GRPC_ADDR"`
-	ElasticSearchAnalyticsHost     string `yaml:"ELASTIC_SEARCH_ANALYTICS_HOST"`
-	ElasticSearchAnalyticsUsername string `yaml:"ELASTIC_SEARCH_ANALYTICS_USERNAME"`
-	ElasticSearchAnalyticsPassword string `yaml:"ELASTIC_SEARCH_ANALYTICS_PASSWORD"`
-	ElasticSearchIndex             string `yaml:"ELASTIC_INDEX"`
+	DBUser                   string `yaml:"DB_USER"`
+	DBPassword               string `yaml:"DB_PASSWORD"`
+	DBPort                   string `yaml:"DB_PORT"`
+	DBHost                   string `yaml:"DB_HOST"`
+	DBName                   string `yaml:"DB_NAME"`
+	KafkaBrokers             string `yaml:"KAFKA_BROKERS"`
+	JWTKeySetURL             string `yaml:"JWT_KEY_SET_URL"`
+	DevicesAPIGRPCAddr       string `yaml:"DEVICES_API_GRPC_ADDR"`
+	ElasticHost              string `yaml:"ELASTIC_HOST"`
+	ElasticUsername          string `yaml:"ELASTIC_USERNAME"`
+	ElasticPassword          string `yaml:"ELASTIC_PASSWORD"`
+	ElasticIndex             string `yaml:"ELASTIC_INDEX"`
+	Port                     string `yaml:"PORT"`
+	TripEventTopic           string `yaml:"TRIP_EVENT_TOPIC"`
+	EthereumSignerPrivateKey string `yaml:"ETHEREUM_SIGNER_PRIVATE_KEY"`
+	BundlrNetwork            string `yaml:"BUNDLR_NETWORK"`
 }
 
 // GetWriterDSN builds the connection string to the db writer - for now same as reader
