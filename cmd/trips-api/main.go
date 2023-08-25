ffeeac7b9e8c5140296a981356d96da9ca20e7b7
Allyson-English <allyson.english@gmail.com>
2023-08-25 14:17:07 -0600

    remove unused

--- cmd/trips-api/main.go
+++ cmd/trips-api/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"os"
 	"os/signal"
@@ -20,10 +19,6 @@ import (
 	"github.com/rs/zerolog"
 )
 
-// const userIDContextKey = "userID"
-
-// var tripStatusCodec = &shared.JSONCodec[kafka.TripStatus]{}
-
 // @title                      DIMO Segment API
 // @version                    1.0
 // @description segments
@@ -39,8 +34,7 @@ func main() {
 	if err != nil {
 		logger.Fatal().Err(err).Msg("Failed loading settings.")
 	}
-	b, _ := json.MarshalIndent(settings, "", " ")
-	fmt.Println(string(b))
+
 	arg := ""
 	if len(os.Args) > 1 {
 		arg = os.Args[1]
@@ -79,48 +73,7 @@ func main() {
 
 		consumer.Start(ctx)
 
-		// tripQueryController := database.NewDatabaseConnection(settings, &logger)
-		// tep := kafka.NewTripEventProcessor(&logger, tripQueryController.Pg)
-		// brokers := strings.Split(settings.KafkaBrokers, ",")
-		// go tep.RunProcessor(ctx, brokers) // press ctrl-c to stop
-
-		// keyRefreshInterval := time.Hour
-		// keyRefreshUnknownKID := true
-		// jwtAuth := jwtware.New(
-		// 	jwtware.Config{
-		// 		KeySetURL:            settings.JWTKeySetURL,
-		// 		KeyRefreshInterval:   &keyRefreshInterval,
-		// 		KeyRefreshUnknownKID: &keyRefreshUnknownKID,
-		// 		SuccessHandler: func(c *fiber.Ctx) error {
-		// 			token := c.Locals("user").(*jwt.Token)
-		// 			claims := token.Claims.(jwt.MapClaims)
-		// 			fmt.Println(claims)
-		// 			c.Locals(userIDContextKey, claims["sub"].(string))
-		// 			return c.Next()
-		// 		},
-		// 		ErrorHandler: func(c *fiber.Ctx, err error) error {
-		// 			return c.Status(fiber.StatusUnauthorized).JSON(
-		// 				map[string]any{
-		// 					"code":    401,
-		// 					"message": "Invalid or expired JWT.",
-		// 				},
-		// 			)
-		// 		},
-		// 	},
-		// )
-
-		// logger.Info().Interface("settings", settings).Msg("Settings")
-
 		app := fiber.New()
-		// app.Get("/swagger/*", swagger.HandlerDefault)
-		// app.Use(cors.New(cors.Config{AllowOrigins: "*"}))
-		// // app.Get("/ongoing/all", tripQueryController.AllOngoingTrips)
-		// // app.Get("/devices/all", tripQueryController.AllUsers)
-
-		// deviceGroup := app.Group("/devices/:id", jwtAuth)
-		// // deviceGroup.Get("/ongoing", tripQueryController.DeviceTripOngoing)
-		// deviceGroup.Get("/alltrips", tripQueryController.AllDeviceTrips)
-
 		app.Get("/health", func(c *fiber.Ctx) error {
 			return c.JSON(map[string]interface{}{
 				"data": "Server is up and running",
@@ -134,16 +87,6 @@ func main() {
 			}
 		}()
 
-		// sigChan := make(chan os.Signal, 1)
-		// signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
-		// sig := <-sigChan
-		// logger.Info().Str("signal", sig.String()).Msg("Got signal, terminating.")
-		// cancel()
-		// <-tep.Done
-		// err = app.Shutdown()
-		// if err != nil {
-		// 	logger.Err(err).Msg("HTTP server did not shut down gracefully.")
-		// }
 		c := make(chan os.Signal, 1)                    // Create channel to signify a signal being sent with length of 1
 		signal.Notify(c, os.Interrupt, syscall.SIGTERM) // When an interrupt or termination signal is sent, notify the channel
 		<-c                                             // This blocks the main thread until an interrupt is received
