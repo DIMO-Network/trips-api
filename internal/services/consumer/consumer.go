8a5c141e6022680e1beeea3d39d760c831a663ee
Allyson-English <allyson.english@gmail.com>
2023-08-28 15:01:47 -0600

    make consumer more generic more multiple events

--- internal/services/consumer/consumer.go
+++ internal/services/consumer/consumer.go
@@ -2,24 +2,23 @@ package consumer
 
 import (
 	"context"
-	"strings"
 	"time"
 
+	pb_devices "github.com/DIMO-Network/devices-api/pkg/grpc"
 	"github.com/DIMO-Network/shared"
 	"github.com/DIMO-Network/shared/kafka"
-	"github.com/DIMO-Network/trips-api/internal/config"
 	"github.com/DIMO-Network/trips-api/internal/services/bundlr"
 	es_store "github.com/DIMO-Network/trips-api/internal/services/es"
 	pg_store "github.com/DIMO-Network/trips-api/internal/services/pg"
 	"github.com/rs/zerolog"
 )
 
-type CompletedSegmentConsumer struct {
-	config kafka.Config
+type Consumer struct {
 	logger *zerolog.Logger
 	es     *es_store.Client
 	pg     *pg_store.Store
 	bundlr *bundlr.Client
+	grpc   pb_devices.UserDeviceServiceClient
 }
 
 type SegmentEvent struct {
@@ -28,24 +27,19 @@ type SegmentEvent struct {
 	DeviceID string    `json:"deviceID"`
 }
 
-func New(es *es_store.Client, bundlrClient *bundlr.Client, pg *pg_store.Store, settings *config.Settings, logger *zerolog.Logger) (*CompletedSegmentConsumer, error) {
-	kc := kafka.Config{
-		Brokers: strings.Split(settings.KafkaBrokers, ","),
-		Topic:   settings.TripEventTopic,
-		Group:   "segmenter",
-	}
-
-	return &CompletedSegmentConsumer{kc, logger, es, pg, bundlrClient}, nil
+func New(es *es_store.Client, bundlrClient *bundlr.Client, pg *pg_store.Store, devicesGRPC pb_devices.UserDeviceServiceClient, logger *zerolog.Logger) *Consumer {
+	return &Consumer{logger, es, pg, bundlrClient, devicesGRPC}
 }
 
-func (c *CompletedSegmentConsumer) Start(ctx context.Context) {
-	if err := kafka.Consume(ctx, c.config, c.ingest, c.logger); err != nil {
-		c.logger.Fatal().Err(err).Msg("Couldn't start segment consumer.")
+func Start(ctx context.Context, config kafka.Config, handler func(context.Context, any) error, logger *zerolog.Logger) {
+	if err := kafka.Consume(ctx, config, handler, logger); err != nil {
+		logger.Fatal().Err(err).Msg("Couldn't start segment consumer.")
 	}
-	c.logger.Info().Msg("segment consumer started.")
+	logger.Info().Msg("segment consumer started.")
 }
 
-func (c *CompletedSegmentConsumer) ingest(ctx context.Context, event *shared.CloudEvent[SegmentEvent]) error {
+func (c *Consumer) CompletedSegment(ctx context.Context, e any) error {
+	event := e.(*shared.CloudEvent[SegmentEvent])
 	response, err := c.es.FetchData(ctx, event.Data.DeviceID, event.Data.Start, event.Data.End)
 	if err != nil {
 		return err
@@ -68,5 +62,12 @@ func (c *CompletedSegmentConsumer) ingest(ctx context.Context, event *shared.Clo
 	// }
 	// upload
 
+	return nil
+}
+
+func (c *Consumer) VehicleEvent(ctx context.Context, e any) error {
+
+	// populate vehicles table: token_id, user_device_id, encryption_key
+	// look at devices-api
 	return nil
 }
