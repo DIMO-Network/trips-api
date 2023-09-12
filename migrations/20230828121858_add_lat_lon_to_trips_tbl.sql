c63c39c34ed80d0225e3f7276c9bb3e076d4f78d
Allyson-English <allyson.english@gmail.com>
2023-09-12 11:10:43 -0600

    add time to start and end cols; start time not null

--- migrations/20230828121858_add_lat_lon_to_trips_tbl.sql
+++ migrations/20230828121858_add_lat_lon_to_trips_tbl.sql
@@ -3,11 +3,19 @@
 SET search_path = trips_api, public;
 
 ALTER TABLE trips
+    RENAME COLUMN "start" TO start_time;
+
+ALTER TABLE trips
+    RENAME COLUMN "end" TO end_time;
+
+ALTER TABLE trips
     ADD COLUMN start_position POINT;
 
 ALTER TABLE trips
     ADD COLUMN end_position POINT;
 
+ALTER TABLE trips
+    ALTER COLUMN start_time SET NOT NULL;
 
 -- +goose StatementEnd
 
