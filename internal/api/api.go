1d4fed4b928e601b325ef81118ef5d52250982e2
Allyson-English <allyson.english@gmail.com>
2023-10-23 13:44:16 -0600

    paginate trips response

--- internal/api/api.go
+++ internal/api/api.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"database/sql"
+	"math"
 	"strconv"
 	"time"
 
@@ -19,7 +20,7 @@ func NewHandler(pgStore *pg_store.Store) *Handler {
 	return &Handler{pgStore}
 }
 
-const maxPageSize = 100
+const pageSize = 100
 
 // Segments godoc
 // @Description details for all segments associated with vehicles
@@ -34,28 +35,48 @@ func (h *Handler) Segments(c *fiber.Ctx) error {
 		return fiber.NewError(fiber.StatusBadRequest, "Couldn't parse vehicle token id.")
 	}
 
+	var p Params
+	err = validateQueryParams(&p, c)
+	if err != nil {
+		return fiber.NewError(fiber.StatusBadRequest, "Couldn't parse query params.")
+	}
+
+	totalCount, err := models.Trips(
+		models.TripWhere.VehicleTokenID.EQ(tokenID),
+	).Count(c.Context(), h.pg.DB)
+	if err != nil {
+		return fiber.NewError(fiber.StatusInternalServerError, err.Error())
+	}
+
+	mods := []qm.QueryMod{
+		qm.OrderBy(models.TripColumns.EndTime + " DESC"),
+		qm.Limit(pageSize),
+		qm.Offset((p.Page - 1) * pageSize),
+	}
+
 	v, err := models.Vehicles(
 		models.VehicleWhere.TokenID.EQ(tokenID),
 		qm.Load(
 			models.VehicleRels.VehicleTokenTrips,
-			qm.OrderBy(models.TripColumns.EndTime+" DESC"),
-			qm.Limit(maxPageSize),
+			mods...,
 		),
 	).One(c.Context(), h.pg.DB)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			return fiber.NewError(fiber.StatusNotFound, "No vehicle with that token id.")
 		}
+		return fiber.NewError(fiber.StatusInternalServerError, err.Error())
 	}
 
-	trips := v.R.VehicleTokenTrips
-
 	out := VehicleTripsResp{
-		Trips: make([]VehicleTripResp, len(trips)),
+		Trips:       make([]VehicleTripResp, len(v.R.VehicleTokenTrips)),
+		CurrentPage: p.Page,
+		TotalPages:  int(math.Ceil(float64(totalCount) / pageSize)),
 	}
 
 	for i, t := range v.R.VehicleTokenTrips {
 		out.Trips[i] = VehicleTripResp{
+			ID: t.ID,
 			Start: PointTime{
 				Time: t.StartTime,
 				Location: Location{
@@ -76,11 +97,30 @@ func (h *Handler) Segments(c *fiber.Ctx) error {
 	return c.JSON(out)
 }
 
+func validateQueryParams(p *Params, c *fiber.Ctx) error {
+	err := c.QueryParser(p)
+	if err != nil {
+		return err
+	}
+
+	if p.Page < 1 {
+		p.Page = 1
+	}
+	return nil
+}
+
+type Params struct {
+	Page int `json:"page"`
+}
+
 type VehicleTripsResp struct {
-	Trips []VehicleTripResp `json:"trips"`
+	Trips       []VehicleTripResp `json:"trips"`
+	TotalPages  int               `json:"totalPages"`
+	CurrentPage int               `json:"currentPage"`
 }
 
 type VehicleTripResp struct {
+	ID    string    `json:"id"`
 	Start PointTime `json:"start"`
 	End   PointTime `json:"end"`
 }
