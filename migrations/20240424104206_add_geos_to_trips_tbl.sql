b5af3c05a62ff87ab37dbae9e55a07e9a0842af2
Allyson-English <allyson.english@gmail.com>
2024-04-24 15:23:34 -0600

    add start and stop location to trip table

---
+++ migrations/20240424104206_add_geos_to_trips_tbl.sql
@@ -0,0 +1,24 @@
+-- +goose Up
+-- +goose StatementBegin
+SET search_path = trips_api, public;
+
+ALTER TABLE trips
+    ADD COLUMN start_position POINT;
+
+ALTER TABLE trips
+    ADD COLUMN end_position POINT;
+
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+
+SET search_path = trips_api, public;
+
+ALTER TABLE trips
+    DROP COLUMN start_position;
+
+ALTER TABLE trips
+    DROP COLUMN end_position;
+
+-- +goose StatementEnd
